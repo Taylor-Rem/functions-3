8d09dc163f4dbe2a321306a836f5d753ee6b93b4
Taylor Remund <remundtaylor3@gmail.com>
2022-12-08 16:15:54 -0700

    applied math.Round() on problem 2

--- arrayMethods.js
+++ arrayMethods.js
@@ -41,7 +41,7 @@ const prices = [15.0, 23.0, 78.0, 34.0, 12.0, 86.0, 12.0, 79.0, 32.0];
 
 // CODE HERE
 const postTaxPrices = prices.map((element) => {
-  return element * 1.07;
+  return Math.round(element * 1.07 * 100) / 100;
 });
 console.log(postTaxPrices);
 
